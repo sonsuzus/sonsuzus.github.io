acc4bef3f320d4c5e97f7b29b50a185973e338fd
Sonsuz Us <52765796+sonsuzus@users.noreply.github.com>
2025-05-04 22:18:03 +0300

    temizle

--- .commitlintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  ignores: [
-    (commit) =>
-      commit.startsWith('Update') || // Genelde dependabot mesajları böyle başlar
-      commit.includes('dependabot')  // dependabot tag'lı commit'leri de atla
-  ],
-};
